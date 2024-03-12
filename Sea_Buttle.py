4906edb84fbedde288be87f7569decc7b0652239
andrew-pier <155960427+andrew-pier@users.noreply.github.com>
2024-03-12 21:59:10 +0300

    Initial commit

--- Sea_Buttle.py
+++ Sea_Buttle.py
@@ -1,20 +1,26 @@
 from tkinter import *
 from tkinter import messagebox
 import time
+import random
 
 tk = Tk()
 app_running = True
 
+#  ОПРЕДЕЛЯЕИ РАЗМЕРЫ ИГРОВОГО ПОЛЯ
 size_canvas_x = 500
 size_canvas_y = 500
-s_x = s_y = 10  # Размер игрового поля
-step_x = size_canvas_x // s_x  # шаг клетки
+s_x = s_y = 10  # Кол-во клеток игрового поля
+step_x = size_canvas_x // s_x  # шаг клетки (размер каждой клетки)
 step_y = size_canvas_y // s_y
+menu_x = size_canvas_x // 2.8  # Место для меню справа от поля
 
-menu_x = 167  # Место для меню справа от поля (размер примерно = Поле_по_Х // 2,4)
+ships_list = [4, 3, 3, 2, 2, 2, 1, 1, 1, 1]  # Список все кораблей
+enemy_ships = [[0 for i in range(s_x)] for i in range(s_y)]  # Пустая матрица игнового поля. Заполняем нолями
+# print('МАТРИЦА ПОЛЯ:\n', *enemy_ships, sep='\n')
+list_ids = []  # список объектов canvas
 
 
-def draw_table():
+def draw_table():  # Рисуем клетки на поле
     for i in range(0, s_x + 1):
         canvas.create_line(i * step_x, 0, i * step_x, size_canvas_y)
     for i in range(0, s_y + 1):
@@ -31,7 +37,7 @@ def on_closing():
 tk.protocol("WM_DELETE_WINDOW", on_closing)  # функция при закрытии окна
 tk.title('Игра "Морской бой"')  # Заголовок окна
 tk.resizable(False, False)  # запрещаем изменение размера окна
-tk.wm_attributes("-topmost", 1)  # поверх окон
+tk.wm_attributes("-topmost", 1)  # поверх других окон
 
 canvas = Canvas(tk, width=size_canvas_x + menu_x, height=size_canvas_y, bd=0, highlightthickness=0)
 canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="white")
@@ -41,19 +47,29 @@ tk.update()
 draw_table()
 
 
+
 def button_show_enemy():
-    pass
+    for i in range(0, s_x):
+        for j in range(0, s_y):
+            if enemy_ships[j][i] > 0:
+                _id = canvas.create_rectangle(i * step_x, j * step_y, i * step_x + step_x, j * step_y + step_y,
+                                              fill="red")
+                list_ids.append(_id)
 
 
 def button_begin_again():
-    pass
+    global list_ids
+    for elemnt in list_ids:
+        canvas.delete(elemnt)
+    list_ids = []
+    generate_enemy_ships(ships_list)
 
 
 b0 = Button(tk, text='Показать корабли противника', command=button_show_enemy)
-b0.place(x=size_canvas_x + 20, y=30, width=127)
+b0.place(x=size_canvas_x + menu_x / 8, y=30, width=menu_x / 4 * 3)  # отступ кнопки 1/8 от меню, ширина = 3/4 от меню
 
 b1 = Button(tk, text='Начать заново!', command=button_begin_again)
-b1.place(x=size_canvas_x + 20, y=60, width=127)
+b1.place(x=size_canvas_x + menu_x / 8, y=60, width=menu_x / 4 * 3)
 
 
 def add_to_all(event):
@@ -61,11 +77,6 @@ def add_to_all(event):
     if event.num == 3:
         _type = 1  # ПКМ
     print(_type)
-
-    # mouse_x1 = event.x
-    # mouse_y1 = event.y
-    # ip_x1 = mouse_x1 // step_x
-    # ip_y1 = mouse_y1 // step_y
 
     mouse_x = canvas.winfo_pointerx() - canvas.winfo_rootx()
     mouse_y = canvas.winfo_pointery() - canvas.winfo_rooty()
@@ -75,12 +86,71 @@ def add_to_all(event):
 
     print(mouse_x, mouse_y)
     print(ip_x, ip_y, '_type: ', _type)
-    # print(ip_x1, ip_y1, '_type: ', _type)
 
 
 canvas.bind_all("<Button-1>", add_to_all)  # Левая кнопка мыши
 canvas.bind_all("<Button-3>", add_to_all)  # Правая кнопка мыши
 
+
+def generate_enemy_ships(ships_list):
+    global enemy_ships
+    enemy_ships = [[0 for i in range(s_x)] for i in range(s_y)]  # Пустая матрица игнового поля. Заполняем нолями
+    count_ships = 0
+
+    while count_ships <= len(ships_list) - 1: # Берём из списка по одному кораблю
+        # i = count_ships
+        ship = ships_list[count_ships] # текущий корабль
+        horizont_vertikal = random.choice(['горизонт', 'вертикаль'])
+        if horizont_vertikal == 'горизонт':
+            a, b = ship - 1, 0
+        else:
+            b, a = ship - 1, 0
+
+        # СЛУЧАЙНЫМ ОБРАЗОМ ГЕНЕРИРУЕМ КООРДИНАТЫ ДЛЯ РАЗМЕЩЕНИЯ КОРАБЛЯ
+        primerno_x = random.randrange(0, s_x)
+        if primerno_x + a > s_x - 1:
+            primerno_x = primerno_x - (primerno_x + a - s_x + 1)  # если точка Х + длина корабля дленее поля,
+            # то смещаем точку Х на лишние клетки
+
+        primerno_y = random.randrange(0, s_y)
+        if primerno_y + b > s_y - 1:
+            primerno_y = primerno_y - (primerno_y + b - s_y + 1)
+
+        x, y = primerno_x, primerno_y
+
+        if x > 0:
+            x_min = x - 1
+        else:
+            x_min = x
+        if x + a < 9:
+            x_max = x + a + 1
+        else:
+            x_max = x + a
+
+        if y > 0:
+            y_min = y - 1
+        else:
+            y_min = y
+        if y + b < 9:
+            y_max = y + b + 1
+        else:
+            y_max = y + b
+
+        # print('Ставим ', count_ships, '-й корабль')
+        # print(ship, '-палубник  x=', x_min, x_max, ' y=', y_min, y_max)
+        test = []  # СПИСОК ПРОВЕРЕННЫХ КЛЕТОК ПОЛЯ
+
+        for n in range(y_min, y_max + 1):
+            for j in range(x_min, x_max + 1):
+                test.append(enemy_ships[n][j])  # ЗАНОСИМ В СПИСОК СОДЕРЖИМОЕ ТЕКУЩЕЙ КЛЕТКИ ПОЛЯ
+        if sum(test) == 0:  # ЕСЛИ ВСЕ ПРОВЕРЕННЫЕ КЛЕТКИ = 0
+            count_ships += 1  # ТО УВЕЛИЧИВАЕМ СЧЁТЧИК НА 1, ДЛЯ ПРОВЕРКИ СЛЕДУЮЩЕГО КОРАБЛЯ
+            # ЗАПИСЫВАЕМ В МАТРИЦЕ ИГРОВОГО ПОЛЯ РАСПОЛОЖЕНИЕ КОРОБЛЯ
+            for n in range(y, y + b + 1):
+                for j in range(x, x + a + 1):
+                    enemy_ships[n][j] = ship
+
+generate_enemy_ships(ships_list)
 
 while app_running:
     if app_running:
